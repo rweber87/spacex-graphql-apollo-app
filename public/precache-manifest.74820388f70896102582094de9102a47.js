4c36b4b866901643d19ad7a2d8cd6157202e062f
Rob Weber <rob.weber87@gmail.com>
2019-02-15 17:30:10 -0500

    working version with graphql and apollo

---
+++ public/precache-manifest.74820388f70896102582094de9102a47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68eb4300a9f4b872b3c45de6aa6951b1",
+    "url": "/static/media/logo.68eb4300.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "88f659ab238938129195",
+    "url": "/static/js/main.88f659ab.chunk.js"
+  },
+  {
+    "revision": "129a2bd43cb7e04f0a20",
+    "url": "/static/js/2.129a2bd4.chunk.js"
+  },
+  {
+    "revision": "88f659ab238938129195",
+    "url": "/static/css/main.16d22f3d.chunk.css"
+  },
+  {
+    "revision": "9d7de52d38b0b69c9a8eb66de31a7302",
+    "url": "/index.html"
+  }
+];
